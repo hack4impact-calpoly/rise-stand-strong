421921bdec3cb4bdd8c976d35609f373c8a29ac6
Jack Fales <46923410+jackfales@users.noreply.github.com>
2021-01-02 11:45:51 -0800

    Added linting and Travis CI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
