1c2cbed67987fab524b57da9088fff8ea97dd225
Jack Fales <46923410+jackfales@users.noreply.github.com>
2021-01-11 11:03:10 -0800

    Added frontend packages; Created login example

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const SubmitButton = styled(Button)`
+  background-color: #6593D6;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  border: none;
+`;
 
 export default () => (
   <div>
     <h1>Login Page</h1>
+    <SubmitButton>Submit</SubmitButton>
   </div>
 );
