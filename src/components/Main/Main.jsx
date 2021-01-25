f4723963920420c32f812893e12e8ce83b2400ad
Pablo Gonzalez <49253760+pgonza25@users.noreply.github.com>
2021-01-25 11:06:43 -0800

    Update src/components/Main/Main.jsx
    Co-authored-by: Jack Fales <46923410+jackfales@users.noreply.github.com>

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 export default () => (
   <div className="App">
     <Switch>
-      <Route exact path="/" render={() => <ForgotPassword />} />
     </Switch>
   </div>
 );
