dfbe78ab069e45766685c2b897c0eadf9b81b654
Pablo Gonzalez <49253760+pgonza25@users.noreply.github.com>
2021-01-25 11:04:56 -0800

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import ForgotPassword from './ForgotPassword/ForgotPassword';
 
 export default () => (
   <div className="App">
