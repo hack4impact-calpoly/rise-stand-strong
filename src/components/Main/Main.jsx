c7ee7a333c1a99417302283e35c6ac90ebfcc1a1
esauerbo <esauerbo@calpoly.edu>
2021-01-27 14:27:23 +1100

    removed routing

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,6 @@ import ForgotPassword from '../ForgotPassword/ForgotPassword';
 import NewAccountPage from '../NewAccountPage/NewAccountPage';
 import EditProfilePage from '../EditProfilePage/EditProfilePage';
 import NavBar from '../NavBar/NavBar';
-import AnnouncementCard from '../AnnouncementCard/AnnouncementCard';
 
 export default () => (
   <div className="App">
@@ -15,7 +14,6 @@ export default () => (
       <Route path="/forgotpassword" render={() => <ForgotPassword />} />
       <Route path="/newaccount" render={() => <NewAccountPage />} />
       <Route path="/editprofile" render={() => <EditProfilePage />} />
-      <Route path="/announcementcard" render={() => <AnnouncementCard />} />
     </Switch>
   </div>
 );
