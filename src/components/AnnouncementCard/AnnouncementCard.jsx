211f5f0970b3283c589ea27aa74ff45c5ea1f7bc
esauerbo <esauerbo@calpoly.edu>
2021-02-04 17:25:45 +1100

    Added props to Annoucenent Card

--- src/components/AnnouncementCard/AnnouncementCard.jsx
+++ src/components/AnnouncementCard/AnnouncementCard.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-export default () => (
+export default (Announcement) => (
   <Card style={{ width: '18rem' }}>
     <Card.Body>
-      <Card.Title>New Protocol</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">March 1, 2021</Card.Subtitle>
+      <Card.Title>{Announcement.title}</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">{Announcement.date}</Card.Subtitle>
       <Card.Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Magne et dolor scelerisqeu tindicunt cars etiam...
+        {Announcement.text}
       </Card.Text>
-      <Card.Link href="#">
+      <Card.Link href={Announcement.link}>
         Read More
       </Card.Link>
     </Card.Body>
