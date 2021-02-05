1bf6df19dadd32ece12f7b6506c806c2e5771a8d
esauerbo <70536670+esauerbo@users.noreply.github.com>
2021-02-04 19:52:11 -0800

    Optimizing styleDate
    Co-authored-by: Jack Fales <46923410+jackfales@users.noreply.github.com>

--- src/components/AnnouncementCard/AnnouncementCard.jsx
+++ src/components/AnnouncementCard/AnnouncementCard.jsx
@@ -3,22 +3,15 @@ import { Card } from 'react-bootstrap';
 
 function styleDate(date) {
   const year = date.split('-')[0];
-  const month = date.split('-')[1];
+  let month = date.split('-')[1];
   const day = date.split('-')[2];
-  if (month === '01') { return (`January ${day}, ${year}`); }
-  if (month === '02') { return (`February ${day}, ${year}`); }
-  if (month === '03') { return (`March ${day}, ${year}`); }
-  if (month === '04') { return (`April ${day}, ${year}`); }
-  if (month === '05') { return (`May ${day}, ${year}`); }
-  if (month === '06') { return (`June ${day}, ${year}`); }
-  if (month === '07') { return (`July ${day}, ${year}`); }
-  if (month === '08') { return (`August ${day}, ${year}`); }
-  if (month === '09') { return (`September ${day}, ${year}`); }
-  if (month === '10') { return (`October ${day}, ${year}`); }
-  if (month === '11') { return (`November ${day}, ${year}`); }
-  if (month === '12') { return (`December ${day}, ${year}`); }
-  return 'Invalid date';
+  const months = [null, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
+  month = months[parseInt(month, 10)];
+  
+  return (`${month} ${day}, ${year}`);
 }
+
 export default (Announcement) => (
   <Card style={{ width: '18rem' }}>
     <Card.Body>
