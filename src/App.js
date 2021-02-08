9fff9b11e964f786a30818a28882d39974e7ebb3
Jack Fales <46923410+jackfales@users.noreply.github.com>
2021-02-07 23:44:43 -0800

    Revert "Added AWS Cognito"
    This reverts commit 811a22b08e23fc998d6ca4887e5427d41a2ef6b7.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
-import Amplify from 'aws-amplify';
 import { withRouter } from 'react-router-dom';
-import awsconfig from './aws-exports';
 import Main from './components/Main/Main';
-
-Amplify.configure(awsconfig);
 
 const App = withRouter(Main);
 
