c45af98e1fe7aeff3d0c189d99025f59d51dc10a
Jack Fales <46923410+jackfales@users.noreply.github.com>
2021-02-07 16:16:40 -0800

    Added announcements endpoints

--- backend/routes/announcements.js
+++ backend/routes/announcements.js
@@ -29,18 +29,43 @@ const router = express.Router();
  *           description: The information displayed in the announcement card.
  *         createdAt:
  *           type: string
- *           format: date
+ *           format: YYYY-MM-DD
  *           description: The date of the record creation.
  *         link:
  *           type: string
  *           description: A link to the announcement details.
  *       example:
+ *          id: 1
  *          title: Valentine's Day Work Schedule
  *          author: John Doe
  *          text: With the upcoming Valentine's day comes an altered work schedule! Click the link to see any changes.
- *          createdAt: 02/01/2021
+ *          createdAt: 2021-01-30
  *          link: https://rise/updates/vday
  */
+
+ /**
+ * @swagger
+ * 
+ * /announcements:
+ *    get:
+ *      summary: Get a list of the 5 most recent announcements
+ *      tags: [Announcements]
+ *      requestBody:
+ *         required: false
+ *      responses:
+ *         "200":
+ *            description: A list of the 5 most recent announcements.
+ *            content:
+ *               application/json:
+ *                  schema:
+ *                     type: array
+ *                     maxItems: 5
+ *                     items:
+ *                        $ref: '#/components/schemas/Announcement'
+ */
+router.get('/', async (req, res) => {
+    res.end();
+})
 
 /**
  * @swagger
@@ -57,11 +82,10 @@ const router = express.Router();
  *                 $ref: '#/components/schemas/Announcement'
  *         responses:
  *             "200":
- *             description: The created announcement.
- *             content:
- *                application/json:
- *                   schema:
- *                   $ref: '#/components/schemas/Announcement'
+ *                description: Announcement created successfully
+ *             "400":
+ *                description: Body not formatted correctly
+ *             
  */
 router.post('/', async (req, res) => {
     res.end();
@@ -71,49 +95,45 @@ router.post('/', async (req, res) => {
  * @swagger
  * 
  * /announcements/{id}:
- *    get:
- *      summary: Get an announcment with the specified id
+ *    put:
+ *      summary: Modify the contents of the specified announcement
  *      tags: [Announcements]
  *      requestBody:
- *         required: false
+ *         required: true
  *         content:
- *           application/json:
- *              schema:
- *                 $ref: '#/components/schemas/Announcement'
- *         responses:
- *             "200":
- *             description: The announcment with the specified id.
- *             content:
- *                application/json:
- *                   schema:
- *                   $ref: '#/components/schemas/Announcement'
+ *            application/json:
+ *               schema:
+ *                  $ref: '#/components/schemas/Announcement'
+ *      responses:
+ *         "200":
+ *            description: Announcement updated successfully
+ *         "403":
+ *            description: Admin privileges required
+ *         "404":
+ *            description: Announcement with the specified id does not exist
  */
-router.get('/:id', async (req, res) => {
+router.put('/', async (req, res) => {
     res.end();
 })
 
 /**
  * @swagger
  * 
- * /announcements:
- *    get:
- *      summary: Get a list of the 5 most recent announcements
+ * /announcements/{id}:
+ *    delete:
+ *      summary: Delete the announcement with the specified id
  *      tags: [Announcements]
  *      requestBody:
  *         required: false
- *         content:
- *           application/json:
- *              schema:
- *                 $ref: '#/components/schemas/Announcement'
- *         responses:
- *             "200":
- *             description: A list of the 5 most recent announcements.
- *             content:
- *                application/json:
- *                   schema:
- *                   $ref: '#/components/schemas/Announcement'
+ *      responses:
+ *         "200":
+ *            description: Announcement deleted successfully
+ *         "403":
+ *            description: Admin privileges required
+ *         "404":
+ *            description: Announcement with the specified id does not exist
  */
-router.get('/', async (req, res) => {
+router.delete('/', async (req, res) => {
     res.end();
 })
 
