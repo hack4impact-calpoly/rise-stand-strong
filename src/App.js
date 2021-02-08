f1e780209a4b9a140fe26873220aa9c61e5830da
Jack Fales <46923410+jackfales@users.noreply.github.com>
2021-02-08 11:03:45 -0800

    Configure Amplify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
+import Amplify from 'aws-amplify';
 import { withRouter } from 'react-router-dom';
+import awsExports from './aws-exports';
 import Main from './components/Main/Main';
+
+Amplify.configure(awsExports);
 
 const App = withRouter(Main);
 
