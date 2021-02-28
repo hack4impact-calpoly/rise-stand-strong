054c6faa4654e383527181ff9686bcdccd905605
Samuel Sehnert <samuel.sehnert@gmail.com>
2021-02-28 12:17:14 -0800

    fixed minor error

--- src/api.js
+++ src/api.js
@@ -6,21 +6,11 @@ const BASE_URL = 'https://1nazz9rwh9.execute-api.us-west-2.amazonaws.com/prod';
 const ANNOUNCEMENTS = '/announcements';
 const SHIFTS = '/shifts';
 
-async function postAnnouncement(userSub, announcementBody) {
+export async function postAnnouncement(userSub, announcementBody) {
 
     await axios.post(`${BASE_URL}/${ANNOUNCEMENTS}`, { ...announcementBody });
 }
 
-async function getAnnouncements(userSub) {
-    await axios.get(`${BASE_URL}/${ANNOUNCEMENTS}`)
-      .catch(function (error) {
-        // handle error
-        console.log("ERROR " + error);
-      });
+export async function getAnnouncements(userSub) {
+    await axios.get(`${BASE_URL}/${ANNOUNCEMENTS}`);
 }
-
-
-module.exports = {
-    postAnnouncement,
-    getAnnouncements
-};
