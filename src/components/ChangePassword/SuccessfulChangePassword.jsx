0659566b6c78ba489e20a93abf0be9b347a1a856
esauerbo <esauerbo@calpoly.edu>
2021-04-29 00:30:17 -0700

    adding successful change page

---
+++ src/components/ChangePassword/SuccessfulChangePassword.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const SubmitButtonContainer = styled.div`
+   text-align: center;
+`;
+const SubmitButtonFullWidth = styled(Button)`
+   font-family: nunito, sans-serif;
+   background-color: #ae4c33;
+   padding: 8px 20px;
+   border-radius: 6px;
+   border: none;
+   line-height: 27px;
+   font-size: 20px;
+   display: flex;
+   justify-content: flex-end;
+   margin-left: auto;
+   margin-top: 50px;
+   margin-right: auto;
+   position: absolute;
+   width: 80%;
+   right: 0;
+   left: 0;
+   text-align: center;
+   display: inline-block;
+   @media only screen and (min-width: 769px) {
+      width: 457px;
+   }
+`;
+
+export default () => {
+   const history = useHistory();
+   return (
+      <div className="success">
+         <p>Password Saved!</p>
+         <a href="/">
+            <SubmitButtonContainer>
+               <SubmitButtonFullWidth 
+                  type="submit"
+                  onClick={history.push('/')}>
+                  Back to Profile
+               </SubmitButtonFullWidth>
+            </SubmitButtonContainer>
+         </a>
+      </div>
+   );
+}
